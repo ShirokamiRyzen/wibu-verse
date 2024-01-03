29e98e6bc57358ec3430c49c7490a852d2f4ba7d
ShirokamiRyzen <infinixgaming360@gmail.com>
2024-01-03 22:16:42 +0700

    UI/UX

--- src/app/nonton/[episodeId]/page.tsx
+++ src/app/nonton/[episodeId]/page.tsx
@@ -11,9 +11,11 @@ interface Episode {
   title: string;
   link: string;
 }
+
 const Page = ({ params }: { params: { episodeId: string } }) => {
-  const [episode, setEpisode] = useState<Episode>();
+  const [episode, setEpisode] = useState<Episode | undefined>();
   const [loading, setLoading] = useState<boolean>(false);
+  const [selectedQuality, setSelectedQuality] = useState<string>('720p');
   const { setLists } = useContext(ListEpisodeContext);
 
   const getEpisode = async () => {
@@ -21,9 +23,7 @@ const Page = ({ params }: { params: { episodeId: string } }) => {
     try {
       const { data } = await axios.get(`/api/episode/${params.episodeId}`);
       setEpisode(data.data);
-      setLoading(false);
     } catch (error: any) {
-      setLoading(false);
       console.log(error.message);
     } finally {
       setLoading(false);
@@ -40,22 +40,50 @@ const Page = ({ params }: { params: { episodeId: string } }) => {
     <div className="py-10">
       {loading ? (
         <div className="flex justify-center">
-          <div className=" rounded-full h-32 w-32">
+
+          <div className="rounded-full h-32 w-32">
             <Image src={loadingGif} alt="loading" width={150} height={150} className="h-full w-full rounded-full" />
           </div>
         </div>
       ) : null}
-      <h1>{episode?.title}</h1>
-      <br />
-      <div className="flex justify-center">
-        <Frame url={episode?.link!} />
-      </div>
-      <span className='flex justify-center'>720p</span>
-      <br />
-      <div className="flex justify-center">
-        <Frame2 url={episode?.link!} />
-      </div>
-      <span className='flex justify-center'>480p</span>
+
+      {episode && (
+        <>
+          <h1>{episode.title}</h1>
+          <br />
+          <div className="alert alert-danger bg-green-500 text-black mx-auto max-w-md" role="alert">
+            <span className="mx-auto">Jika reso 720p burik atau error, berarti link gada</span>
+          </div>
+          <br />
+          <div className="flex justify-center">
+            {selectedQuality === '720p' ? (
+              <Frame url={episode.link} />
+            ) : (
+              <Frame2 url={episode.link} />
+            )}
+          </div>
+          <span className='flex justify-center'>Kualitas saat ini: {selectedQuality}</span>
+          <br />
+
+          {/* Add buttons for quality selection */}
+          <div className="flex justify-center mt-3 rounded">
+            <div className="border border-black p-2">
+              <button
+                onClick={() => setSelectedQuality('720p')}
+                className={`mr-3 ${selectedQuality === '720p' ? 'bg-green-500 text-white' : 'bg-blue-500 text-black'} py-2 px-4 rounded`}
+              >
+                720p
+              </button>
+              <button
+                onClick={() => setSelectedQuality('480p')}
+                className={`${selectedQuality === '480p' ? 'bg-green-500 text-white' : 'bg-blue-500 text-black'} py-2 px-4 rounded`}
+              >
+                480p
+              </button>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
