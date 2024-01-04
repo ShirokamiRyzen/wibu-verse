4fb0a0b61c7235673efc7a0900141973660a46f9
ShirokamiRyzen <infinixgaming360@gmail.com>
2024-01-04 10:16:48 +0700

    update link

--- src/components/Frame.tsx
+++ src/components/Frame.tsx
@@ -1,23 +1,8 @@
 import React from 'react';
 
 const Frame = ({ url }: { url: string }) => {
-  // Memeriksa apakah url ada sebelum mencoba melakukan penggantian
-  if (!url) {
-    return <div>Tunggu sebentar...</div>;
-  }
 
-  // Mengganti bagian url yang diinginkan
-  let modifiedUrl = url;
-
-  if (url.includes('https://desustream.me/desudesu/?id=')) {
-    modifiedUrl = url.replace('https://desustream.me/desudesu/?id=', 'https://desustream.me/desudesuhd/?id=');
-  } else if (url.includes('https://desustream.me/stream/?id=')) {
-    modifiedUrl = url.replace('https://desustream.me/stream/?id=', 'https://desustream.me/stream/hd/?id=');
-  } else if (url.includes('https://desustream.me/beta/stream/?id=')) {
-    modifiedUrl = url.replace('https://desustream.me/beta/stream/?id=', 'https://desustream.me/beta/stream/hd/?id=');
-  }
-
-  return <iframe src={modifiedUrl} height={200} allowFullScreen></iframe>;
+  return <iframe src={url} height={200} allowFullScreen></iframe>;
 };
 
 export default Frame;
