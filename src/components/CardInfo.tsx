71c0f3bb1ced78a609db924b0a1af7be5ee1caaa
ShirokamiRyzen <infinixgaming360@gmail.com>
2024-01-04 11:48:27 +0700

    Improvement

--- src/components/CardInfo.tsx
+++ src/components/CardInfo.tsx
@@ -11,20 +11,19 @@ interface CardInfoProps {
 
 const CardInfo = ({ id, title, thumbnail, genres }: CardInfoProps) => {
   return (
-    <div className="card p-0 card-side bg-base-100 shadow-xl min-h-fit">
-      <figure className="w-36">
-        <Image src={thumbnail} width={160} height={680} alt={title} />
-      </figure>
-      <div className="w-10 p-2  card-body ">
-        <h2 className="card-title text-base ">{title}</h2>
-        <p className="text-xs">Genre: {genres.join(', ')}</p>
-        <div className="card-actions justify-end ">
-          <Link href={`/${id}`} className=" bg-zinc-800 text-base p-2 rounded-lg text-white">
-            Tonton
-          </Link>
+    <Link href={`/${id}`} passHref>
+      <div className="card p-0 card-side bg-gray-800 shadow-xl min-h-fit">
+        <figure className="w-36">
+          <Image src={thumbnail} width={160} height={680} alt={title} />
+        </figure>
+        <div className="w-10 p-2  card-body ">
+          <h2 className="card-title text-base ">{title}</h2>
+          <p className="text-xs">Genre: {genres.join(', ')}</p>
+          <div className="card-actions justify-end">
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
