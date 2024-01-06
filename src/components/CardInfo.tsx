6e18b3ad03c017d85aab8e0a7ad082da9eb6b746
ShirokamiRyzen <infinixgaming360@gmail.com>
2024-01-06 17:37:08 +0700

    modified:   src/components/CardInfo.tsx

--- src/components/CardInfo.tsx
+++ src/components/CardInfo.tsx
@@ -10,15 +10,18 @@ interface CardInfoProps {
 }
 
 const CardInfo = ({ id, title, thumbnail, genres }: CardInfoProps) => {
+
+  const darkMode = true;
+
   return (
     <Link href={`/${id}`} passHref>
-      <div className="card p-0 card-side bg-main-800 shadow min-h-fit">
+      <div className="card p-0 card-side bg-gray-800 shadow-xl min-h-fit">
         <figure className="w-36">
           <Image src={thumbnail} width={160} height={680} alt={title} />
         </figure>
-        <div className="w-10 p-2  card-body ">
-          <h2 className="card-title text-base ">{title}</h2>
-          <p className="text-xs">Genre: {genres.join(', ')}</p>
+        <div className="w-10 p-2 card-body">
+          <h2 className={`card-title ${darkMode ? 'text-white' : 'text-black'}`}>{title}</h2>
+          <p className={`text-xs ${darkMode ? 'text-white' : 'text-black'}`}>Genre: {genres.join(', ')}</p>
           <div className="card-actions justify-end">
           </div>
         </div>
