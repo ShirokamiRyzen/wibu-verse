418581407394d08947f134f13e8bf4e889957286
ShirokamiRyzen <infinixgaming360@gmail.com>
2024-01-20 21:37:03 +0700

    modified:   src/components/Frame.tsx

--- src/components/Frame.tsx
+++ src/components/Frame.tsx
@@ -1,28 +1,8 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 
 const Frame = ({ url }: { url: string }) => {
-  const iframeRef = useRef<HTMLIFrameElement>(null);
 
-  useEffect(() => {
-    const iframe = iframeRef.current;
-    if (iframe) {
-      iframe.onload = () => {
-        try {
-          const iframeDoc = iframe.contentDocument || iframe.contentWindow?.document;
-          if (iframeDoc) {
-            const adsElement = iframeDoc.querySelector('.box_item_ads_popup');
-            if (adsElement) {
-              adsElement.remove();
-            }
-          }
-        } catch (error) {
-          console.error('Error accessing iframe contents:', error);
-        }
-      };
-    }
-  }, [url]);
-
-  return <iframe ref={iframeRef} src={url} width={480} height={240} allowFullScreen></iframe>;
+  return <iframe src={url} width={480} height={240} allowFullScreen></iframe>;
 };
 
 export default Frame;
