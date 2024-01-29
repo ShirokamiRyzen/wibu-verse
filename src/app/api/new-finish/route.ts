31a16ffb4195cf1bfbc0a441a1b2a4d0d0d0b602
ShirokamiRyzen <infinixgaming360@gmail.com>
2024-01-29 20:39:29 +0700

    testing wide result

--- src/app/api/new-finish/route.ts
+++ src/app/api/new-finish/route.ts
@@ -3,38 +3,41 @@ import { NextRequest, NextResponse } from 'next/server';
 import cheerio from 'cheerio';
 
 export const GET = async (req: NextRequest) => {
-  const timestamps = [1, 2, 3, 4];
-
+  const pages = [1, 2, 3, 4];
   try {
-    const fetchPromises = timestamps.map(async (timestamp) => {
-      const { data, status } = await useFetch(`https://otakudesu.media/complete-anime/page/${timestamp}/`);
+    const requests = pages.map(async (page) => {
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      const { data, status } = await useFetch(`https://otakudesu.media/complete-anime/page/${page}`);
+
       if (status !== 200) throw new Error(`Error ${status}`);
-      return data;
-    });
 
-    const responses = await Promise.all(fetchPromises);
-
-    const combinedList = responses.flatMap((data, index) => {
       const $ = cheerio.load(data);
       const $parentElement = $('#venkonten > div > div.venser > div.venutama > div.rseries > div > div.venz > ul > li');
 
-      return $parentElement.map((i, el) => {
-        const title = $(el).find('div > div.thumb > a > div > h2').text();
-        const thumbnail = $(el).find('div > div.thumb > a > div > img').attr('src');
-        const episode = $(el).find('div > div.epz').text();
-        const temp = $(el).find('div > div.thumb > a').attr('href')?.split('/');
+      const list = $parentElement
+        .map((i, el) => {
+          const title = $(el).find('div > div.thumb > a > div > h2').text();
+          const thumbnail = $(el).find('div > div.thumb > a > div > img').attr('src');
+          const episode = $(el).find('div > div.epz').text();
+          const temp = $(el).find('div > div.thumb > a').attr('href')?.split('/');
 
-        return {
-          id: temp?.[temp.length - 2],
-          title,
-          thumbnail,
-          episode,
-        };
-      }).get();
+          return {
+            id: temp?.[temp.length - 2],
+            title,
+            thumbnail,
+            episode,
+          };
+        })
+        .get();
+
+      return list;
     });
 
-    return NextResponse.json({ status: 200, success: true, data: combinedList }, { status: 200 });
+    const results = await Promise.all(requests);
+    const mergedList = results.flat(); // Menggabungkan hasil dari semua halaman
+
+    return NextResponse.json({ status: 200, success: true, data: mergedList }, { status: 200 });
   } catch (error: any) {
-    return NextResponse.json({ msg: error }, { status: 500 });
+    return NextResponse.json({ msg: error.message }, { status: 500 });
   }
 };
